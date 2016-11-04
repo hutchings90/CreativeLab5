5bf82b1e890368602471d1155ccd1f6e6281dbbe
hutchings90 <hutchings90@gmail.com>
2016-11-04 04:21:53 +0000

    Login page, google maps

---
+++ maps/public/javascripts/app.js
@@ -0,0 +1,12 @@
+angular.module('LoginApp', [])
+.controller('LoginController', [
+	'$scope', '$http', '$window',
+	function($scope, $http, $window){
+		$scope.startSession = function(){
+//			This will open a new tab with directions from google maps
+//			source first, destination second
+//			$window.open("https://www.google.com/maps/dir/40.7608+-111.891/40.2235984+-111.7374736");
+			return $http.get().success();
+		};
+	}
+]);
--- maps/routes/index.js
+++ maps/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+router.get('user/:user', function(req, res, next){
+	
+	console.log("At a user");
 });
 
 module.exports = router;
